da92dcecbf396a099eb0b6290a3c8a436dd2e549
Mohamed Chbinou <92753150+DivineSpeed@users.noreply.github.com>
2025-05-27 15:51:26 +0100

    Update lancer_kruskal.bat

--- lancer_kruskal.bat
+++ lancer_kruskal.bat
@@ -1,12 +1,12 @@
 @echo off
-echo Installation des dépendances en cours...
+echo Installation des dependances en cours...
 pip install -r requirements.txt
 if %ERRORLEVEL% NEQ 0 (
-    echo Erreur lors de l'installation des dépendances. Veuillez vérifier que Python est correctement installé.
+    echo Erreur lors de l'installation des dependances. Veuillez verifier que Python est correctement installe.
     pause
     exit /b
 )
-echo Démarrage de l'Application Kruskal...
+echo Demarrage de l'Application Kruskal...
 python application_kruskal.py
 if %ERRORLEVEL% NEQ 0 (
     echo Erreur lors du lancement de l'application.
